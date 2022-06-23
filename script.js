8ab312ef29b4ced31aab41b721eaad7545cd5c03
Saurabh <saurabh1258.be21@chitkarauniversity.edu.in>
2022-06-24 00:42:04 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let hour = document.getElementById("hour");
+let minutes = document.getElementById("min");
+let seconds = document.getElementById("sec");
+let timing =  setInterval(() => {
+    let myDate = new Date();
+    let hr = myDate.getHours();
+    let mins = myDate.getMinutes();
+    let secs = myDate.getSeconds();
+
+    let rot_hr = (hr*30) + (mins/2);
+    let rot_m = (mins*6) + (secs/10);
+    let rot_s = secs*6;
+
+    hour.style.transform = `rotate(${rot_hr}deg)`;
+    minutes.style.transform = `rotate(${rot_m}deg)`;
+    seconds.style.transform = `rotate(${rot_s}deg)`;
+    
+}, 1000);
+
+function darkMode() {
+
+    var img = document.getElementById('dayn').src;
+if (img.indexOf('https://cdn-icons-png.flaticon.com/512/3917/3917805.png')!=-1) {
+    document.getElementById('dayn').src  = 'https://cdn-icons.flaticon.com/png/512/2920/premium/2920682.png?token=exp=1656009002~hmac=b3596f3d5ec906a3d949af4dacc11154';
+}
+ else {
+   document.getElementById('dayn').src = 'https://cdn-icons-png.flaticon.com/512/3917/3917805.png';
+}
+        
+    let dcircle = document.querySelector(".circle");
+    dcircle.classList.toggle("darkCircle");
+    let dbg = document.querySelector(".light");
+    dbg.classList.toggle("bg");
+
+
+    
+   
+}
